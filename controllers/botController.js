60caf0ffa0c0eac0d1eecd299b56b13996420c95
thesekaiheiwa <52303726+thesekaiheiwa@users.noreply.github.com>
2019-10-07 20:30:29 +0900

    MongoDB documentIDの変更

--- controllers/botController.js
+++ controllers/botController.js
@@ -33,7 +33,7 @@ exports.storyBot = (req, res, next) => {
 	let userName = req.user.name;
 	console.log(progress, context);
 
-	Story.findById("5d984039f7b405a13073aa19")
+	Story.findById("5d9b20587c213e55613b1693")
 		.then(stories => {
 			// fetchしたストーリーが終わったら、終了する内容を返す
 			if (progress < stories.output.length) {
